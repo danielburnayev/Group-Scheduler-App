257fa20abde8bd5cf15b95f9aca2380e1b2a66db
danielburnayev <137850713+danielburnayev@users.noreply.github.com>
2024-09-03 14:51:16 -0400

    Setup with XCode project

---
+++ Group Scheduler App/Group Scheduler App/Group_Scheduler_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_Scheduler_AppApp.swift
+//  Group Scheduler App
+//
+//  Created by Daniel Burnayev on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Group_Scheduler_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Group Scheduler App/Group Scheduler AppUITests/Group_Scheduler_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Group_Scheduler_AppUITestsLaunchTests.swift
+//  Group Scheduler AppUITests
+//
+//  Created by Daniel Burnayev on 9/3/24.
+//
+
+import XCTest
+
+final class Group_Scheduler_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
