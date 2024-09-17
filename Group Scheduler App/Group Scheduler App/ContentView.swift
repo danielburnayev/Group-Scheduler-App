45ea20636fc025dee0dab316f98ce62d9c55644f
dburnaye <dburnaye@gmu.edu>
2024-09-17 17:29:55 -0400

    Initial design and adjustable text/buttons

--- Group Scheduler App/Group Scheduler App/ContentView.swift
+++ Group Scheduler App/Group Scheduler App/ContentView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Button("WOW!!!") {
-                print("printed")
+        GeometryReader { proxy in
+            VStack(spacing: proxy.size.height / 25.3) {
+                Text("Scheduler App")
+                    .font(.system(size: proxy.size.width / 6.55))
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, proxy.size.height / 8)
+                
+                Button("New Schedule") {
+                    let now = Date()
+                    let formatter = DateFormatter()
+                    formatter.timeZone = TimeZone.current
+                    formatter.dateFormat = "yyyy-MM-dd EEE"
+                    let dateString = formatter.string(from: now)
+                    print(dateString)
+                }
+                .padding(25)
+                .background(Color(red: 217/255, green: 217/255, blue: 52/255, opacity: 1.0))
+                .foregroundColor(Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.85))
+                .font(.system(size: proxy.size.width / 13.1)) //og font size 30
+                .cornerRadius(12.5)
+                
+                Button("Preivous Schedules") {
+                    print("Pull up previous schedules \(proxy.size.width)x\(proxy.size.height)")
+                }
+                .padding(25.0)
+                .background(Color.green)
+                .foregroundColor(Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.85))
+                .font(.system(size: proxy.size.width / 13.1)) //og font size 30
+                .cornerRadius(12.5)
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .padding()
+            .background(Color(red: 152/255, green: 255/255, blue: 177/255, opacity: 1.0))
         }
-        .padding()
     }
 }
 
